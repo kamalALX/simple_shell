d61374b6b82d771600d096bb3bc1161f6e0a3df6
Kamal <kamalmiftah01@gmail.com>
2023-12-14 00:34:39 +0100

    fixing task 8: built-in exit

--- simple_shell.c
+++ simple_shell.c
@@ -170,7 +170,7 @@ int exit_comand(char **input_tokens, char *shell_name)
 				else
 				{
 					write(2, shell_name, _strlen(shell_name));
-					write(2, ": exit doesn't take more than one argument\n", 43);
+					write(2, ": exit: too many arguments\n", 27);
 					break;
 				}
 			}
